246480497fa5d9af3743601e7f7775306b4fdb2c
syedzaheerbukhari01-netizen <syedzaheerbukhari01@gmail.com>
2026-09-09 22:36:28 +0500

    Update question 5.py

--- question 5.py
+++ question 5.py
@@ -2,15 +2,12 @@ age = 20
 item = 5
 print("age : ",age)
 print("items : ", item)
-
 age = float(age)
 item = float(item)
 print("age : ",age)
 print("items: ", item)
-#what does this code do
-#it creates two variables age and item storing integers 20 and 5
-#it prints their original integer values
-#it converts both variables to floating-point numbers using float()
-#it prints the converted values, showing decimals (20.0 and 5.0)
-#actully value is same but data type is defferent
-#regards from AZAN THEBO (CR)
+#explaing code 
+#code creates 2 variables age and item stors integers 20 and 5
+#prints its original integer value like 20 and 5
+#and converts both variables to floating point numbers using float()
+#then prints the converted values showing decimals (20.0 and 5.0)
